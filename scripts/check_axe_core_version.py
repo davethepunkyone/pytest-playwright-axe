a198f0ad8400c7117e7143184d9e81080125b6cd
Dave Harding <35965578+davethepunkyone@users.noreply.github.com>
2025-04-11 15:17:19 +0100

    Test changes to confirm workflow works as expected

--- scripts/check_axe_core_version.py
+++ scripts/check_axe_core_version.py
@@ -1,15 +1,17 @@
-import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
-
 import os
 import requests
 import logging
-from src.pytest_playwright_axe import __version__ as package_version
+import tomllib
+
+
+TOML_PATH = Path(os.getcwd()) / "pyproject.toml"
 
 
 def axe_core_update_required() -> bool:
     """Check the axe-core version in the repository."""
+
+    # Check axe-core version
     url = "https://api.github.com/repos/dequelabs/axe-core/releases/latest"
     response = requests.get(url)
     if response.status_code == 200:
@@ -17,8 +19,13 @@ def axe_core_update_required() -> bool:
         logging.info(f"Latest axe-core version: {latest_version}")
     else:
         logging.error("Failed to fetch the latest axe-core version from GitHub.")
+
+    with open(TOML_PATH, "rb") as f:
+        toml_data = tomllib.load(f)
     
-    current_version = str(f"v{package_version}" if "-" not in package_version else f"v{package_version.split('-')[0]}")
+    toml_version = toml_data["project"]["version"]
+    
+    current_version = str(f"v{toml_version}" if "-" not in toml_version else f"v{toml_version.split('-')[0]}")
     logging.info(f"Current axe-core version: {current_version}")
     
     result = current_version != latest_version
