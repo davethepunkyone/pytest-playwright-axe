4d0b6eabffa65eab479497ce3d1e6a32c2d77582
Dave Harding <35965578+davethepunkyone@users.noreply.github.com>
2025-05-04 21:42:22 +0100

    Support for getRules

--- tests/test_example.py
+++ tests/test_example.py
@@ -14,3 +14,8 @@ def test_minified_example(page: Page) -> None:
 
     # Assert repo text is present
     Axe.run(page, options=OPTIONS_WCAG_22AA, use_minified_file=True)
+
+
+def test_get_rules(page: Page) -> None:
+    rules = Axe.get_rules(page, ["wcag2a"])
+    print(rules[0])
