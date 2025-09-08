7a929d7c1b8ea2c5b28e6627f71236ad973e93f8
Dave Harding <35965578+davethepunkyone@users.noreply.github.com>
2025-09-08 22:17:46 +0100

    Add skip to report of report test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from playwright.sync_api import Page
+import pytest
 from src.pytest_playwright_axe import Axe, OPTIONS_WCAG_22AA
 
 
@@ -22,6 +23,7 @@ def test_get_rules(page: Page) -> None:
     print(rules[0])
 
 
+@pytest.mark.skip(reason="Test is for local execution only")
 def test_get_report_of_report(page: Page) -> None:
     EXAMPLES_DIR = Path(__file__).parent.parent / "examples" / "example_result_report.html"
     page.goto(str(EXAMPLES_DIR))
