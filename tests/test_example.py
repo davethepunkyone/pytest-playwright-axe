3f60d331690f2506c831c30a498b702143be1349
Dave Harding <35965578+davethepunkyone@users.noreply.github.com>
2025-10-21 21:34:14 +0100

    Snapshot support not in yet

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,14 +4,11 @@ import pytest
 from src.pytest_playwright_axe import Axe, OPTIONS_WCAG_22AA
 
 
-SNAPSHOT_PATH = Path(__file__).parent.parent.joinpath("snapshots")
-
-
 def test_basic_example(page: Page) -> None:
     page.goto("https://github.com/davethepunkyone/pytest-playwright-axe")
 
     # Assert repo text is present
-    Axe(snapshot_directory=SNAPSHOT_PATH).run(page, options=OPTIONS_WCAG_22AA)
+    Axe().run(page, options=OPTIONS_WCAG_22AA)
 
 
 def test_minified_example(page: Page) -> None:
